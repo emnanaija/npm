b9cc3974d42dc01b2ad6dae9b4a60a946dd45330
emna <emna.naija@esprit.tn>
2023-08-18 20:03:43 +0200

    new

---
+++ index.js
@@ -0,0 +1,10 @@
+//var generateName = require('sillyname');
+/*import generateName from "sillyName";
+var sillyName = generateName();
+
+console.log(`my name is ${ sillyName} `);
+*/
+import superheroes from "superheroes";
+var name= superheroes.random();
+
+console.log(`i am  ${ name} `);
